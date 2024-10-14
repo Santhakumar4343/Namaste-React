8793af0c5d2e8693722447b9ce54c3c57d4bb4cc
arunmadana <arunkumarmadana@gmail.com>
2024-10-14 18:32:23 +0530

    initial commit

---
+++ App.js
@@ -0,0 +1,25 @@
+
+
+const parent =React.createElement(
+    "div",
+    {id:"parent"},
+   [ React.createElement("div",
+    {id:"child1"},
+    [ React.createElement("h1",{},
+        "iam h1 tag"
+    ), React.createElement("h2",{},
+        "iam h2 tag"
+    )]
+), React.createElement("div",
+    {id:"child1"},
+    [ React.createElement("h1",{},
+        "iam h1 tag"
+    ), React.createElement("h2",{},
+        "iam h2 tag"
+    )]
+)]
+)
+
+const root=ReactDOM.createRoot(document.getElementById("root"))
+
+root.render(parent)
